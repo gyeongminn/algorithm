e180eaa30c5de83d0ba542a83749bc7af8e0d462
Gyeongmin Lee <97784561+gyeongminn@users.noreply.github.com>
2022-07-05 00:00:25 +0900

    [Bronze I] Title: 이항 계수 1, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -1,15 +1,11 @@
-def factorial(K):
-    if K < 2:
-        return 1
-    else:
-        return K * factorial(K-1)
+from math import factorial
 
-N, K = map(int, input().split())
+n, k = map(int, input().split())
 
 num1 = 1
-for i in range(K):
-    num1 *= N-i
+for i in range(k):
+    num1 *= n - i
 
-num2 = factorial(K)
+num2 = factorial(k)
 
 print(num1 // num2)
