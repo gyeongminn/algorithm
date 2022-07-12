2f331af25d33ee720f3b614e1e63aacb1ab5c953
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2022-07-12 17:33:30 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.py
@@ -0,0 +1,8 @@
+n = int(input())
+
+for i in range(n-1, 0, -1):
+    print(' ' * (n - i - 1), end='')
+    print('*' * (2 * i + 1))
+for i in range(n):
+    print(' ' * (n - i - 1), end='')
+    print('*' * (2 * i + 1))
