93c7a25b5298b9777b55b06779d86882c30712e3
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2022-12-17 23:31:58 +0900

    [Bronze II] Title: 완전제곱수, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/1977. 완전제곱수/완전제곱수.py
+++ 백준/Bronze/1977. 완전제곱수/완전제곱수.py
@@ -1,23 +1,10 @@
 n = int(input())
 m = int(input())
 
+a = [i ** 2 for i in range(1, 101) if n <= i ** 2 <= m]
 
-def sqrt(n: int):
-    return int(n ** 0.5)
-
-
-square = [x**2 for x in range(1, 101)]
-
-min = 0
-sum = 0
-for i in range(n, m+1):
-    if i in square:
-        if sum == 0:
-            min = i
-        sum += i
-        
-if sum == 0:
+if len(a) == 0:
     print(-1)
 else:
-    print(sum)
-    print(min)
+    print(sum(a))
+    print(a[0])
