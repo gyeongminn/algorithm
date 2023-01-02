e22d826ec97438d4b84aa9f10ca58d7cd1827fec
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2023-01-02 23:34:32 +0900

    [Silver II] Title: 친구, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1058. 친구/친구.py
+++ 백준/Silver/1058. 친구/친구.py
@@ -1,27 +1,17 @@
-from itertools import combinations
-
 n = int(input())
-
-distance_1 = [[] * x for x in range(n)]
-distance_2 = [[] * x for x in range(n)]
-count = [0 for _ in range(n)]
+friends = [[] for _ in range(n)]
 
 for i in range(n):
-    arr = list(input())
+    tmp = list(input())
     for j in range(n):
-        if i == j:
-            continue
-        if arr[j] == 'Y':
-            distance_1[i].append(j)
+        if tmp[j] == 'Y':
+            friends[i].append(j)
 
+max_friends = 0
 for i in range(n):
-    for a, b in list(combinations(distance_1[i], 2)):
-        if a not in distance_1[b]:
-            distance_2[b].append(a)
-        if b not in distance_1[a]:
-            distance_2[a].append(b)
-
-result = 0
-for i in range(n):
-    result = max(len(distance_1[i]) + len(set(distance_2[i])), result)
-print(result)
+    tmp = []
+    for j in friends[i]:
+        tmp += friends[j]
+    max_friends = max(max_friends, len(set(tmp + friends[i])) - 1)
+        
+print(max_friends)
