45e9104ccf122b2b6ce9c59f9f0d9b83ce0a6140
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2023-01-02 23:09:54 +0900

    [Silver II] Title: 친구, Time: 56 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1058. 친구/친구.py
+++ 백준/Silver/1058. 친구/친구.py
@@ -2,27 +2,26 @@ from itertools import combinations
 
 n = int(input())
 
-arr = []
-friends = [[] * x for x in range(n)]
-friends2 = [[] * x for x in range(n)]
+distance_1 = [[] * x for x in range(n)]
+distance_2 = [[] * x for x in range(n)]
 count = [0 for _ in range(n)]
 
 for i in range(n):
-    arr.append(list(input()))
+    arr = list(input())
     for j in range(n):
         if i == j:
-            continue  # 자기자신은 패스
-        if arr[i][j] == 'Y':
-            friends[i].append(j)
+            continue
+        if arr[j] == 'Y':
+            distance_1[i].append(j)
 
 for i in range(n):
-    for a, b in list(combinations(friends[i], 2)):
-        if a not in friends[b] and a not in friends2[b]:
-            friends2[b].append(a)
-        if b not in friends[a] and b not in friends2[a]:
-            friends2[a].append(b)
+    for a, b in list(combinations(distance_1[i], 2)):
+        if a not in distance_1[b]:
+            distance_2[b].append(a)
+        if b not in distance_1[a]:
+            distance_2[a].append(b)
 
 result = 0
 for i in range(n):
-    result = max(len(friends[i]) + len(friends2[i]), result)
+    result = max(len(distance_1[i]) + len(set(distance_2[i])), result)
 print(result)
