160bcbc530c959d773c41beccccd2c286107746b
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2023-01-06 17:09:52 +0900

    [Silver III] Title: 통계학, Time: 308 ms, Memory: 148868 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -1,28 +1,29 @@
-def mode(arr):
-    dic = {}
-    for num in arr:
-        if num in dic:
-            dic[num] += 1
-        else:
-            dic[num] = 1
-
-    max_count = max(dic.values())
-    mode_arr = []
-    for i in dic:
-        if max_count == dic[i]:
-            mode_arr.append(i)
-
-    if len(mode_arr) == 1:
-        return mode_arr[0]
-    return mode_arr[1]
-
-
+import sys
 n = int(input())
-arr = []
-for _ in range(n):
-    arr.append(int(input()))
+l = []
 
-print(round(sum(arr) / len(arr))) # 산술평균
-print(sorted(arr)[n // 2]) # 중앙값
-print(mode(arr)) # 최빈값
-print(max(arr)-min(arr)) # 범위
+for _ in range(n):
+    l.append(int(sys.stdin.readline()))
+
+l.sort()
+
+print('%d'%(round(sum(l)/n, 0)))                            # 산술평균
+print(l[n//2])                                              # 중앙값
+
+x = {}                                                      # 딕셔너리 생성
+
+for i in l:                                                 # 빈도수 저장
+    if i in x:
+        x[i] += 1
+    else:
+        x[i] = 1
+x = sorted(x.items(), key=lambda x : x[1], reverse = True)  # 빈도수를 기준으로 오름차순 정렬
+
+if n != 1: 
+    if x[0][1] == x[1][1]:                                  # 최빈값이 여러 개 있다면 최빈값 중 두번째로 작은값 출력
+        print(x[1][0])
+    else: print(x[0][0])
+else:
+    print(x[0][0])
+
+print(max(l) - min(l))                                      # 범위
