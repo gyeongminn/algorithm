3d0914fdf3b2e0df03465a9bea3b46700ed35c8f
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2023-02-06 16:00:27 +0900

    [Silver I] Title: 배열 돌리기 3, Time: 288 ms, Memory: 121972 KB -BaekjoonHub

--- 백준/Silver/16935. 배열 돌리기 3/배열 돌리기 3.py
+++ 백준/Silver/16935. 배열 돌리기 3/배열 돌리기 3.py
@@ -8,25 +8,25 @@ options = [int(x) for x in input().split()]
 
 for option in options:
     if option == 1:
-        arr = arr[::-1]
+        arr = arr[::-1]  # 상하 반전
     elif option == 2:
-        arr = list(map(lambda x: x[::-1], arr))
+        arr = list(map(lambda x: x[::-1], arr))  # 좌우 반전
     elif option == 3:
-        arr = list(zip(*arr[::-1]))
-        n, m = m, n
+        arr = list(zip(*arr[::-1]))  # 오른쪽으로 90도 회전
+        n, m = m, n  # 행, 열 사이즈 스왑
     elif option == 4:
-        arr = list(map(list, zip(*arr)))[::-1]
+        arr = list(map(list, zip(*arr)))[::-1]  # 왼쪽으로 90도 회전
         n, m = m, n
     else:
-        n_2, m_2 = n//2, m//2
-        arr1 = map(lambda x: x[:m_2], arr[:n-n_2])
-        arr2 = map(lambda x: x[m_2:], arr[:n-n_2])
-        arr3 = map(lambda x: x[:m-m_2], arr[n_2:])
-        arr4 = map(lambda x: x[m_2:], arr[n_2:])
-        if option == 5:
-            arr = list(map(lambda x: x[0]+x[1], list(zip(arr3, arr1)) + list(zip(arr4, arr2))))
-        elif option == 6:
-            arr = list(map(lambda x: x[0]+x[1], list(zip(arr2, arr4)) + list(zip(arr1, arr3))))
+        arr1 = map(lambda x: x[:m//2], arr[:n//2])  # 1사분면
+        arr2 = map(lambda x: x[m//2:], arr[:n//2])  # 2사분면
+        arr3 = map(lambda x: x[:m//2], arr[n//2:])  # 3사분면
+        arr4 = map(lambda x: x[m//2:], arr[n//2:])  # 4사분면
+        if option == 5:  # 시계 방향 사분면 회전
+            arr = list(zip(arr3, arr1)) + list(zip(arr4, arr2))
+        elif option == 6:  # 반시계 방향 사분면 회전
+            arr = list(zip(arr2, arr4)) + list(zip(arr1, arr3))
+        arr = list(map(lambda x: x[0]+x[1], arr))
 
 for row in arr:
     print(*row)
