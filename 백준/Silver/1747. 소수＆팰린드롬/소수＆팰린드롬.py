2547714bdd4b1649ac161abaac89b423348137a8
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2023-02-13 03:27:50 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 232 ms, Memory: 117312 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
@@ -1,23 +1,44 @@
-import math
+import sys
+import random
+
+input = sys.stdin.readline
 
 
-def is_prime(n: int):
-    if n == 1:
+def is_prime(n):
+    if n <= 1 or n == 4:
         return False
-    for i in range(2, int(math.sqrt(n))+1):
-        if n % i == 0:
+    if n <= 3:
+        return True
+
+    d = n - 1
+    s = 0
+    while d % 2 == 0:
+        d //= 2
+        s += 1
+
+    def try_composite(a):
+        if pow(a, d, n) == 1:
             return False
+        for i in range(s):
+            if pow(a, 2**i * d, n) == n - 1:
+                return False
+        return True
+
+    for i in range(5):
+        a = random.randint(2, n - 1)
+        if try_composite(a):
+            return False
+
     return True
 
 
-def is_palindrome(n: int):
-    return str(n) == str(n)[::-1]
+def is_palindrome(num):
+    num_str = str(num)
+    return num_str == num_str[::-1]
 
 
 n = int(input())
-
-while True:
-    if is_palindrome(n) and is_prime(n):
-        print(n)
+for num in range(n, 100000000):
+    if is_palindrome(num) and is_prime(num):
+        print(num)
         break
-    n += 1
