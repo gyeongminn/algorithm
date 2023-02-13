e356899572c84d27d02388f06e82fa6de1981b33
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2023-02-13 17:04:32 +0900

    [Gold I] Title: 구간 곱 구하기, Time: 684 ms, Memory: 138536 KB -BaekjoonHub

--- 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.py
+++ 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.py
@@ -2,22 +2,15 @@ from math import ceil, log2
 import sys
 
 
-def init_tree():
-    for i in range(N):
-        tree[size + i] = int(input())
-    for i in range(size-1, 0, -1):
-        tree[i] = tree[i*2] * tree[i*2+1] % MOD
-
-
 def mul(start, end, left, right, node):
     if right < start or left > end:
         return 1
-    
+
     if left <= start and right >= end:
         return tree[node]
 
     mid = (start + end) // 2
-    return mul(start, mid, left, right, node*2) * mul(mid+1, end, left, right, node*2+1) % MOD
+    return mul(start, mid, left, right, node*2) * mul(mid+1, end, left, right, node*2+1) % mod
 
 
 def update(start, end, node, index, value):
@@ -27,23 +20,27 @@ def update(start, end, node, index, value):
     if start == end:
         tree[node] = value
         return
-    
+
     mid = (start + end) // 2
     update(start, mid, node*2, index, value)
     update(mid+1, end, node*2+1, index, value)
-    tree[node] = tree[node*2] * tree[node*2+1] % MOD
+    tree[node] = tree[node*2] * tree[node*2+1] % mod
 
 
 input = sys.stdin.readline
+mod = 1000000007
 
-N, M, K = map(int, input().split())
-MOD = 1000000007
+n, m, k = map(int, input().split())
 
-size = 2 ** ceil(log2(N))
+size = 2 ** ceil(log2(n))
 tree = [0] * (size * 2)
-init_tree()
 
-for _ in range(M + K):
+for i in range(n):
+    tree[size + i] = int(input())
+for i in range(size-1, 0, -1):
+    tree[i] = tree[i*2] * tree[i*2+1] % mod
+
+for _ in range(m + k):
     a, b, c = map(int, input().split())
     if a == 1:
         update(1, size-1, 1, b, c)
