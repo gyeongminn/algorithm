014d3324db85f346a1ea92bc5e342f9bbed8606b
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2023-02-13 16:55:36 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,13 +1,7 @@
-import sys
-
-input = sys.stdin.readline
 test_case = [int(input()) for _ in range(int(input()))]
-
 dp = [0 for _ in range(12)]
 dp[1:5] = 1, 2, 4, 7
-
 for i in range(5, 12):
     dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
-
 for n in test_case:
     print(dp[n])
