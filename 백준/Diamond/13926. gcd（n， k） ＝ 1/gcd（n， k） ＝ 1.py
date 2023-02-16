e7ecf936903889d7aa424bae7352a20dfecf452f
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2023-02-16 17:30:07 +0900

    [Diamond V] Title: gcd(n, k) = 1, Time: 172 ms, Memory: 117344 KB -BaekjoonHub

---
+++ 백준/Diamond/13926. gcd（n， k） ＝ 1/gcd（n， k） ＝ 1.py
@@ -0,0 +1,90 @@
+import sys
+import random
+import math
+from itertools import combinations
+
+input = sys.stdin.readline
+
+
+def is_prime(n):
+    if n <= 1 or n == 4:
+        return False
+    if n <= 3:
+        return True
+
+    d = n - 1
+    s = 0
+    while d % 2 == 0:
+        d //= 2
+        s += 1
+
+    def try_composite(a):
+        if pow(a, d, n) == 1:
+            return False
+        for i in range(s):
+            if pow(a, 2**i * d, n) == n-1:
+                return False
+        return True
+
+    for _ in range(5):
+        a = random.randint(2, n-1)
+        if try_composite(a):
+            return False
+
+    return True
+
+
+def pollard_rho(n):
+    def g(x):
+        return (x * x + 1) % n
+    start_x, start_y, d = 2, 2, 1
+    for k in range(2, 11):
+        x = pow(start_x, k)
+        y = pow(start_y, k)
+        while d == 1:
+            x = g(x)
+            y = g(g(y))
+            d = math.gcd(abs(x - y), n)
+        if 1 < d < n:
+            return d
+    return d
+
+
+def factorize(n):
+    factor = 2
+    while (factor**2 <= n):
+        while (n % factor == 0):
+            factors.append(factor)
+            n = n // factor
+        factor += 1
+    if n > 1:
+        factors.append(n)
+
+
+N = int(input())
+
+n = N
+factors = []
+while n != 1:
+    if is_prime(n):
+        factors.append(n)
+        break
+    k = pollard_rho(n)
+    factors.append(int(k))
+    n = n // k
+for _ in range(len(factors)):
+    for x in factors:
+        if not is_prime(x):
+            factors.remove(x)
+            factorize(x)
+
+result = N
+factors = set(factors)
+for i in range(len(factors)):
+    for j in combinations(factors, i + 1):
+        k = 1
+        for l in j:
+            k *= l
+        result = result + N // k if i % 2 else result - N // k
+
+print(result)
