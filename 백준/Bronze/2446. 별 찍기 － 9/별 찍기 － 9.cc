f3257f3e2cb6bacb6e8e05e5dbc3bd9533b74aa6
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2023-02-22 16:12:24 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0)
+using namespace std;
+
+int main(void) {
+    fastio;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << ' ';
+        }
+        for (int j = 0; j < (n - i) * 2 - 1; j++) {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+    for (int i = n - 2; i >= 0; i--) {
+        for (int j = 0; j < i; j++) {
+            cout << ' ';
+        }
+        for (int j = 0; j < (n - i) * 2 - 1; j++) {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+}
