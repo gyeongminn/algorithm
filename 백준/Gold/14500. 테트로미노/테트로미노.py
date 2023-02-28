a53dd4be354e525beca317219ae274f4e9502562
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2023-03-01 04:26:03 +0900

    [Gold IV] Title: 테트로미노, Time: 216 ms, Memory: 118072 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -6,7 +6,6 @@ n, m = map(int, input().split())
 arr = [[int(x) for x in input().split()] for _ in range(n)]
 result = 0
 
-# oooo
 for y in range(n):
     for x in range(m-3):
         if result < arr[y][x]+arr[y][x+1]+arr[y][x+2]+arr[y][x+3]:
@@ -15,88 +14,45 @@ for y in range(n-3):
     for x in range(m):
         if result < arr[y][x]+arr[y+1][x]+arr[y+2][x]+arr[y+3][x]:
             result = arr[y][x]+arr[y+1][x]+arr[y+2][x]+arr[y+3][x]
-# oo
-# oo
 for y in range(n-1):
     for x in range(m-1):
         if result < arr[y][x]+arr[y][x+1] +arr[y+1][x]+arr[y+1][x+1]:
             result = arr[y][x]+arr[y][x+1] +arr[y+1][x]+arr[y+1][x+1]
-# o
-# o
-# oo
-for y in range(n-2):
-    for x in range(m-1):
-        if result < arr[y][x]+arr[y+1][x]+arr[y+2][x]+arr[y+2][x+1]:
-            result = arr[y][x]+arr[y+1][x]+arr[y+2][x]+arr[y+2][x+1]
 for y in range(n-1):
     for x in range(m-2):
         if result < arr[y][x]+arr[y+1][x]+arr[y][x+1]+arr[y][x+2]:
             result = arr[y][x]+arr[y+1][x]+arr[y][x+1]+arr[y][x+2]
-for y in range(n-2):
-    for x in range(m-1):
-        if result < arr[y][x]+arr[y][x+1]+arr[y+1][x+1]+arr[y+2][x+1]:
-            result = arr[y][x]+arr[y][x+1]+arr[y+1][x+1]+arr[y+2][x+1]
-for y in range(n-1):
-    for x in range(m-2):
-        if result < arr[y+1][x]+arr[y+1][x+1]+arr[y+1][x+2]+arr[y][x+2]:
-            result = arr[y+1][x]+arr[y+1][x+1]+arr[y+1][x+2]+arr[y][x+2]
-#  o
-#  o
-# oo
-for y in range(n-2):
-    for x in range(m-1):
-        if result < arr[y][x+1]+arr[y+1][x+1]+arr[y+2][x+1]+arr[y+2][x]:
-            result = arr[y][x+1]+arr[y+1][x+1]+arr[y+2][x+1]+arr[y+2][x]
-for y in range(n-1):
-    for x in range(m-2):
-        if result < arr[y][x]+arr[y+1][x]+arr[y+1][x+1]+arr[y+1][x+2]:
-            result = arr[y][x]+arr[y+1][x]+arr[y+1][x+1]+arr[y+1][x+2]
-for y in range(n-2):
-    for x in range(m-1):
-        if result < arr[y][x]+arr[y+1][x]+arr[y+2][x]+arr[y][x+1]:
-            result = arr[y][x]+arr[y+1][x]+arr[y+2][x]+arr[y][x+1]
-for y in range(n-1):
-    for x in range(m-2):
-        if result < arr[y][x]+arr[y][x+1]+arr[y][x+2]+arr[y+1][x+2]:
-            result = arr[y][x]+arr[y][x+1]+arr[y][x+2]+arr[y+1][x+2]
-# o
-# oo
-#  o
-for y in range(n-2):
-    for x in range(m-1):
-        if result < arr[y][x]+arr[y+1][x]+arr[y+1][x+1]+arr[y+2][x+1]:
-            result = arr[y][x]+arr[y+1][x]+arr[y+1][x+1]+arr[y+2][x+1]
-for y in range(n-1):
-    for x in range(m-2):
-        if result < arr[y+1][x]+arr[y+1][x+1]+arr[y][x+1]+arr[y][x+2]:
-            result = arr[y+1][x]+arr[y+1][x+1]+arr[y][x+1]+arr[y][x+2]
-for y in range(n-1):
-    for x in range(m-2):
-        if result < arr[y][x]+arr[y+1][x+1]+arr[y][x+1]+arr[y+1][x+2]:
-            result = arr[y][x]+arr[y+1][x+1]+arr[y][x+1]+arr[y+1][x+2]
-        result = max(result, arr[y][x]+arr[y+1][x+1]+arr[y][x+1]+arr[y+1][x+2])
-for y in range(n-2):
-    for x in range(m-1):
-        if result < arr[y+1][x]+arr[y][x+1]+arr[y+1][x+1]+arr[y+2][x]:
-            result = arr[y+1][x]+arr[y][x+1]+arr[y+1][x+1]+arr[y+2][x]
-
-# ooo
-#  o
-for y in range(n-1):
-    for x in range(m-2):
         if result < arr[y][x]+arr[y][x+1]+arr[y][x+2]+arr[y+1][x+1]:
             result = arr[y][x]+arr[y][x+1]+arr[y][x+2]+arr[y+1][x+1]
-for y in range(n-2):
-    for x in range(m-1):
-        if result < arr[y][x]+arr[y+1][x]+arr[y+2][x]+arr[y+1][x+1]:
-            result = arr[y][x]+arr[y+1][x]+arr[y+2][x]+arr[y+1][x+1]
-for y in range(n-1):
-    for x in range(m-2):
         if result < arr[y+1][x]+arr[y+1][x+1]+arr[y+1][x+2]+arr[y][x+1]:
             result = arr[y+1][x]+arr[y+1][x+1]+arr[y+1][x+2]+arr[y][x+1]
+        if result < arr[y][x]+arr[y+1][x+1]+arr[y][x+1]+arr[y+1][x+2]:
+            result = arr[y][x]+arr[y+1][x+1]+arr[y][x+1]+arr[y+1][x+2]
+        if result < arr[y+1][x]+arr[y+1][x+1]+arr[y][x+1]+arr[y][x+2]:
+            result = arr[y+1][x]+arr[y+1][x+1]+arr[y][x+1]+arr[y][x+2]
+        if result < arr[y][x]+arr[y][x+1]+arr[y][x+2]+arr[y+1][x+2]:
+            result = arr[y][x]+arr[y][x+1]+arr[y][x+2]+arr[y+1][x+2]
+        if result < arr[y][x]+arr[y+1][x]+arr[y+1][x+1]+arr[y+1][x+2]:
+            result = arr[y][x]+arr[y+1][x]+arr[y+1][x+1]+arr[y+1][x+2]
+        if result < arr[y+1][x]+arr[y+1][x+1]+arr[y+1][x+2]+arr[y][x+2]:
+            result = arr[y+1][x]+arr[y+1][x+1]+arr[y+1][x+2]+arr[y][x+2]
 for y in range(n-2):
     for x in range(m-1):
+        if result < arr[y][x]+arr[y+1][x]+arr[y+2][x]+arr[y+2][x+1]:
+            result = arr[y][x]+arr[y+1][x]+arr[y+2][x]+arr[y+2][x+1]
+        if result < arr[y][x]+arr[y][x+1]+arr[y+1][x+1]+arr[y+2][x+1]:
+            result = arr[y][x]+arr[y][x+1]+arr[y+1][x+1]+arr[y+2][x+1]
+        if result < arr[y][x]+arr[y+1][x]+arr[y+2][x]+arr[y+1][x+1]:
+            result = arr[y][x]+arr[y+1][x]+arr[y+2][x]+arr[y+1][x+1]
         if result < arr[y+1][x]+arr[y][x+1]+arr[y+1][x+1]+arr[y+2][x+1]:
             result = arr[y+1][x]+arr[y][x+1]+arr[y+1][x+1]+arr[y+2][x+1]
+        if result < arr[y+1][x]+arr[y][x+1]+arr[y+1][x+1]+arr[y+2][x]:
+            result = arr[y+1][x]+arr[y][x+1]+arr[y+1][x+1]+arr[y+2][x]
+        if result < arr[y][x]+arr[y+1][x]+arr[y+1][x+1]+arr[y+2][x+1]:
+            result = arr[y][x]+arr[y+1][x]+arr[y+1][x+1]+arr[y+2][x+1]
+        if result < arr[y][x]+arr[y+1][x]+arr[y+2][x]+arr[y][x+1]:
+            result = arr[y][x]+arr[y+1][x]+arr[y+2][x]+arr[y][x+1]
+        if result < arr[y][x+1]+arr[y+1][x+1]+arr[y+2][x+1]+arr[y+2][x]:
+            result = arr[y][x+1]+arr[y+1][x+1]+arr[y+2][x+1]+arr[y+2][x]
 
 print(result)
