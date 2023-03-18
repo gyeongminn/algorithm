b9f83b1656e1729c597c779ac0fb7e75edd9b020
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2023-03-18 20:14:37 +0900

    [Bronze II] Title: 완전제곱수, Time: 112 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Bronze/1977. 완전제곱수/완전제곱수.py
+++ 백준/Bronze/1977. 완전제곱수/완전제곱수.py
@@ -1,10 +1,15 @@
 n = int(input())
 m = int(input())
 
-a = [i*i for i in range(1, 101) if n <= i*i <= m]
+square = [x*x for x in range(1, 101)]
 
-if len(a) == 0:
+arr = []
+for i in range(n, m+1):
+    if i in square:
+        arr.append(i)
+        
+if arr == []:
     print(-1)
 else:
-    print(sum(a))
-    print(a[0])
+    print(sum(arr))
+    print(arr[0])
