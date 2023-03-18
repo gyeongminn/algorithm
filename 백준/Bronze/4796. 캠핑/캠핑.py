0c5fe212e4f03f1a4a9cb850ce6fb55bc3da44f1
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2023-03-18 17:22:46 +0900

    [Bronze I] Title: 캠핑, Time: 192 ms, Memory: 114844 KB -BaekjoonHub

---
+++ 백준/Bronze/4796. 캠핑/캠핑.py
@@ -0,0 +1,12 @@
+case = 0
+while True:
+    l, p, v = map(int, input().split())
+    if l == p == v == 0:
+        break
+    if v % p > l:
+        result = v // p * l + l
+    else:
+        result = v // p * l + v % p
+        
+    case += 1
+    print("Case %d: %d" %(case, result))
