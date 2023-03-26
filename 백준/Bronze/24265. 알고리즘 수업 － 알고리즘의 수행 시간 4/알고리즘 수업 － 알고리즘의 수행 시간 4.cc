528031fcf63788dae73c9aa7c35e433ee629427f
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2023-03-26 17:36:10 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#define endl '\n'
+using namespace std;
+
+int main(void) {
+    fastio;
+    int n;
+    cin >> n;
+    cout << 1L * n * (n-1) / 2 << endl << 2;
+}
