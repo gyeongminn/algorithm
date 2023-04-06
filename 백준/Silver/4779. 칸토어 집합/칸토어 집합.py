adc4f1b0d24634fe2dacbf75c18804cafa8d6926
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2023-04-06 17:32:50 +0900

    [Silver III] Title: 칸토어 집합, Time: 112 ms, Memory: 118200 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -5,12 +5,12 @@ input = sys.stdin.readline
 def sol(n):
     if n == 0:
         return '-'
-    return sol(n-1) + ' ' * pow(3, n-1) + sol(n-1)
-    
-    
+    prev = sol(n-1)
+    return prev + ' ' * pow(3, n-1) + prev
+
+
 while True:
     try:
-        n = int(input())
+        print(sol(int(input())))
     except:
         break
-    print(sol(n))
