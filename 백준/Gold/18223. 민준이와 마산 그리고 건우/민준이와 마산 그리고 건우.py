1668840824c4de39140219549a4ebee220c55df3
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2023-04-14 18:26:50 +0900

    [Gold IV] Title: 민준이와 마산 그리고 건우, Time: 292 ms, Memory: 119432 KB -BaekjoonHub

---
+++ 백준/Gold/18223. 민준이와 마산 그리고 건우/민준이와 마산 그리고 건우.py
@@ -0,0 +1,35 @@
+import sys
+from heapq import heappop, heappush
+input = sys.stdin.readline
+INF = sys.maxsize
+
+
+def dijkstra(graph, start):
+    n = len(graph)
+    dist = [INF] * n
+    dist[start] = 0
+    queue = [(0, start)]
+    while queue:
+        path_len, v = heappop(queue)
+        if path_len == dist[v]:
+            for w, edge_len in graph[v]:
+                if edge_len + path_len < dist[w]:
+                    dist[w] = edge_len + path_len
+                    heappush(queue, (edge_len + path_len, w))
+    return dist
+
+
+v, e, x = map(int, input().split())
+graph = [[] for _ in range(v+1)]
+for _ in range(e):
+    a, b, w = map(int, input().split())
+    graph[a].append((b, w))
+    graph[b].append((a, w))
+
+dist = dijkstra(graph, 1)
+dist_x = dijkstra(graph, x)
+
+if dist[v] == dist[x] + dist_x[v]:
+    print("SAVE HIM")
+else:
+    print("GOOD BYE")
