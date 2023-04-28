740b689a5160ec5517886a15b22629f6dde525a3
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2023-04-28 18:31:25 +0900

    [Silver IV] Title: 청기 백기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/15736. 청기 백기/청기 백기.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+
+int main(void) {
+    fastio;
+    int n;
+    cin >> n;
+    cout << floor(sqrt(n));
+}
