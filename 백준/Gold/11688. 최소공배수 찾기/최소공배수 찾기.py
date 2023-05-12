e06cb756ff84a9b1d4493f5529480833fc298ee7
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2023-05-12 18:30:26 +0900

    [Gold IV] Title: 최소공배수 찾기, Time: 132 ms, Memory: 34660 KB -BaekjoonHub

---
+++ 백준/Gold/11688. 최소공배수 찾기/최소공배수 찾기.py
@@ -0,0 +1,60 @@
+from collections import Counter
+from math import *
+import sys
+input = sys.stdin.readline
+
+
+def pollard_rho(n):
+    if n & 1 == 0:
+        return 2
+    if n % 3 == 0:
+        return 3
+
+    s = ((n - 1) & (1 - n)).bit_length() - 1
+    d = n >> s
+    for a in [2, 325, 9375, 28178, 450775, 9780504, 1795265022]:
+        p = pow(a, d, n)
+        if p == 1 or p == n - 1 or a % n == 0:
+            continue
+        for _ in range(s):
+            prev = p
+            p = (p * p) % n
+            if p == 1:
+                return gcd(prev - 1, n)
+            if p == n - 1:
+                break
+        else:
+            for i in range(2, n):
+                x, y = i, (i * i + 1) % n
+                f = gcd(abs(x - y), n)
+                while f == 1:
+                    x, y = (x * x + 1) % n, (y * y + 1) % n
+                    y = (y * y + 1) % n
+                    f = gcd(abs(x - y), n)
+                if f != n:
+                    return f
+    return n
+
+
+def all_factors(n):
+    small, large = [], []
+    for i in range(1, int(n**0.5) + 1, 2 if n & 1 else 1):
+        if not n % i:
+            small.append(i)
+            large.append(n // i)
+    if small[-1] == large[-1]:
+        large.pop()
+    large.reverse()
+    small.extend(large)
+    return small
+
+
+a, b, l = map(int, input().split())
+
+t = lcm(a, b)
+for c in all_factors(l):
+    if lcm(t, c) == l:
+        print(c)
+        break
+else:
+    print(-1)
