a5e515af81a3bd209b231d0db45d055e0215b648
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2023-05-17 19:12:47 +0900

    [Silver II] Title: 리그 오브 레전설 (Small), Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/17271. 리그 오브 레전설 （Small）/리그 오브 레전설 （Small）.py
+++ 백준/Silver/17271. 리그 오브 레전설 （Small）/리그 오브 레전설 （Small）.py
@@ -2,16 +2,24 @@ import sys
 input = sys.stdin.readline
 mod = 1000000007
 
-def nCr(n, r):
-    return fac[n] * pow(fac[r] * fac[n - r] % mod, mod - 2, mod) % mod
 
-fac = [1 for _ in range(10001)]
-for i in range(2, 10001):
-    fac[i] = fac[i - 1] * i % mod
+def make_nCr_mod(max_n, mod):
+    fac = [1 for _ in range(max_n+1)]
+    for i in range(2, max_n+1):
+        fac[i] = fac[i - 1] * i % mod
+        
+    def nCr_mod(n, r):
+        return fac[n] * pow(fac[r] * fac[n - r] % mod, mod - 2, mod) % mod
+    
+    return nCr_mod
 
+
+nCr = make_nCr_mod(10000, 1000000007)
 n, m = map(int, input().split())
 ans = 0
+
 for b in range(n // m+1):
-    a = n - b *m
+    a = n - b * m
     ans += nCr(a+b, b)
+    
 print(ans % mod)
