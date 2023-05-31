7075bd7caf7f0a9ebe1c7c5024b79a55e7b0211a
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2023-05-31 15:42:47 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#define endl '\n'
+using namespace std;
+
+int n, m;
+int arr[10];
+bool visited[10];
+
+void solve(int depth) {
+    if (depth == m) {
+        for (int i = 1; i < m; ++i) {
+            if (arr[i] <= arr[i - 1]) {
+                return;
+            }
+        }
+        for (int i = 0; i < m; ++i) {
+            cout << arr[i] << ' ';
+        }
+        cout << endl;
+        return;
+    }
+    for (int i = 1; i <= n; ++i) {
+        if (!visited[i]) {
+            arr[depth] = i;
+            visited[i] = true;
+            solve(depth + 1);
+            visited[i] = false;
+        }
+    }
+}
+
+int main() {
+    fastio;
+    cin >> n >> m;
+    solve(0);
+}
