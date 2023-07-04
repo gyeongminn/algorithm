d16775c483730b56dce9c6cdf7bbe5c9734447b2
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2023-07-05 00:13:51 +0900

    [Bronze IV] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define ll long long
+
+using namespace std;
+
+int main() {
+    fastio;
+    ll a, b;
+    cin >> a >> b;
+    if (a == b) {
+        cout << 1;
+    } else {
+        cout << 0;
+    }
+    return 0;
+}
