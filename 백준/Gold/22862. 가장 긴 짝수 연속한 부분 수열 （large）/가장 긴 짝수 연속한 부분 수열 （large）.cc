62cc30d2031ab606ff988994fea02e0ee1363812
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2023-07-12 12:23:30 +0900

    [Gold V] Title: 가장 긴 짝수 연속한 부분 수열 (large), Time: 108 ms, Memory: 6316 KB -BaekjoonHub

---
+++ 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.cc
@@ -0,0 +1,30 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#include <bits/stdc++.h>
+#define ll long long
+#define endl '\n'
+using namespace std;
+
+int N, K;
+int A[1100100];
+
+int main() {
+	cin.tie(nullptr)->sync_with_stdio(false);
+	cin >> N >> K;
+	for (int i = 1; i <= N; i++) cin >> A[i];
+
+	int ans = 0, cnt = 0;
+	int e = 0;
+	for (int s = 1; s <= N; ++s) {
+		while (e + 1 <= N && cnt <= K) {
+			if (A[e + 1] % 2) cnt++;
+			e++;
+			if (cnt <= K) ans = max(ans, e - s + 1 - cnt);
+		}
+		if (cnt <= K) ans = max(ans, e - s + 1 - cnt);
+		if (A[s] % 2) cnt--;
+	}
+
+	cout << ans;
+	return 0;
+}
