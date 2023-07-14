b8d2d19af0cc8d8ee2256d6b4c7e9fe002e51cb8
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2023-07-15 01:46:16 +0900

    [Bronze IV] Title: SASA 모형을 만들어보자, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23825. SASA 모형을 만들어보자/SASA 모형을 만들어보자.cc
@@ -0,0 +1,16 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#include <bits/stdc++.h>
+
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define endl '\n'
+using namespace std;
+
+int main() {
+    fastio;
+    int n, m;
+    cin >> n >> m;
+    cout << min(n / 2, m / 2);
+    return 0;
+}
