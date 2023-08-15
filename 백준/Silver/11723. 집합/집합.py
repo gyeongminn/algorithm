bc98b263eee600099b10e66afe03942da6898343
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2023-08-16 02:19:28 +0900

    [Silver V] Title: 집합, Time: 4616 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,36 +1,29 @@
 import sys
-from collections import defaultdict
-
 input = sys.stdin.readline
 
-dic = defaultdict(bool)
-empty = defaultdict(bool)
-all = defaultdict(bool)
-for i in range(1, 21):
-    all[str(i)] = True
-
+s = set()
 n = int(input())
+
 for _ in range(n):
     query = input().split()
-    if query[0] == "add":
-        n = query[1]
-        dic[n] = True
-    elif query[0] == "remove":
-        n = query[1]
-        dic[n] = False
-    elif query[0] == "check":
-        n = query[1]
-        if dic[n] == True:
-            print(1)
-        else:
-            print(0)
-    elif query[0] == "toggle":
-        n = query[1]
-        if dic[n] == True:
-            dic[n] = False
-        else:
-            dic[n] = True
-    elif query[0] == "all":
-        dic = all
+    if query[0] == "all":
+        s = set([i for i in range(1, 21)])
+        continue
     elif query[0] == "empty":
-        dic = empty
+        s = set()
+        continue
+    n = int(query[1])
+    try:
+        if query[0] == "add":
+            s.add(n)
+        elif query[0] == "remove":
+            s.remove(n)
+        elif query[0] == "check":
+            print(1 if n in s else 0)
+        elif query[0] == "toggle":
+            if n in s:
+                s.discard(n)
+            else:
+                s.add(n)
+    except:
+        pass
