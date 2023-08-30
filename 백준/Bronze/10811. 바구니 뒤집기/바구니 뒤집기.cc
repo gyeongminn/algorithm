2c9da30f985703fd52bbc76baea7b926ed848baf
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2023-08-31 01:16:21 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
@@ -1,23 +1,21 @@
 #include <bits/stdc++.h>
-#define ll long long
-#define fastio ios::sync_with_stdio(0), cin.tie(0)
 using namespace std;
 
-int main(void) {
-    fastio;
-    int n, m, a, b;
-    vector<int> v;
-    cin >> n >> m;
-    for (int i = 1; i <= n; i++) {
-        v.push_back(i);
-    }
-    while (m--) {
-        cin >> a >> b;
-        for (int i = 0; i <= (b - a) / 2; i++) {
-            swap(v[a + i - 1], v[b - i - 1]);
+int N, M, L, R;
+int A[101];
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+    cin >> N >> M;
+    for (int i = 1; i <= N; ++i) A[i] = i;
+
+    while (M--) {
+        cin >> L >> R;
+        for (int i = 0; i <= (R - L) / 2; ++i) {
+            swap(A[L + i], A[R - i]);
         }
     }
-    for (int i : v) {
-        cout << i << ' ';
-    }
+
+    for (int i = 1; i <= N; ++i) cout << A[i] << ' ';
+    return 0;
 }
