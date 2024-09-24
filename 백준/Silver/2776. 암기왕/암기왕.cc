0cedaf7f7b502505a26dced4edc195a9d0750081
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2024-09-24 22:31:43 +0900

    [Silver IV] Title: 암기왕, Time: 948 ms, Memory: 44620 KB -BaekjoonHub

---
+++ 백준/Silver/2776. 암기왕/암기왕.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n, m, v;
+    unordered_set<int> s;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cin >> v;
+        s.insert(v);
+    }
+    cin >> m;
+    for (int i = 0; i < m; ++i) {
+        cin >> v;
+        cout << (s.count(v) ? 1 : 0) << '\n';
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
