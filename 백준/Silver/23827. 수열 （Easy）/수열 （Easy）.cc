b86fa863c64b4352972f9d7bfdc7d5db1a018b5c
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2025-02-14 11:32:14 +0900

    [Silver IV] Title: 수열 (Easy), Time: 44 ms, Memory: 3976 KB -BaekjoonHub

---
+++ 백준/Silver/23827. 수열 （Easy）/수열 （Easy）.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int &i : v) cin >> i;
+
+    long long ans = 0, sum = 0;
+    for (int i : v) {
+        ans += sum * i;
+        sum += i;
+        ans %= 1000000007;
+        sum %= 1000000007;
+    }
+
+    cout << ans;
+}
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+    solve();
+    return 0;
+}
