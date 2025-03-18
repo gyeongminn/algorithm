f2b1c74807926919ec3b38fa654d57c1212410f1
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2025-03-18 15:04:36 +0900

    [Bronze II] Title: 완전제곱수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1977. 완전제곱수/완전제곱수.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int m, n;
+    scanf("%d %d", &m, &n);
+
+    int min = 999999999, sum = 0;
+    for (int i = 1; i * i <= 10000; i++) {
+        int num = i * i;
+        if (num < m || num > n) {
+            continue;
+        }
+
+        sum += num;
+        if (min > num) {
+            min = num;
+        }
+    }
+
+    if (sum == 0) {
+        printf("-1");
+        return 0;
+    }
+    
+    printf("%d\n", sum);
+    printf("%d\n", min);
+
+    return 0;
+}
