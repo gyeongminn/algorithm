b1da51b097886a66f8832951751a53414ad99bb1
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2025-03-22 11:52:20 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.c
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.c
@@ -1,18 +1,47 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <string.h>
+#define TARGET_SIZE 8
+
+char list[TARGET_SIZE][4] = { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
+
+int check(const char* begin, const char* target)
+{
+    int index = 0;
+    while (target[index]) {
+        if (begin[index] == target[index]) {
+            index++;
+        } else {
+            return 0;
+        }
+    }
+
+    return index;
+}
 
 int main()
 {
-	int sum;
-	char arr[100];
-	scanf("%s", &arr);
-	sum = strlen(arr);
-	for (int i = 0; i < strlen(arr); i++)
-	{
-		if ((arr[i] == 'l' || arr[i] == 'n') && arr[i + 1] == 'j') sum--; 
-		if (arr[i] == 'd' && arr[i + 1] == 'z' && arr[i + 2] == '=') sum--; 
-		if (arr[i] == '=' || arr[i] == '-') sum--;
-	}
-	printf("%d", sum);
+    char str[101];
+    scanf("%s", str);
+
+    int count = 0;
+    for (char* p1 = str; *p1; p1++) {
+        int is_matched = 0;
+        for (int i = 0; i < TARGET_SIZE; i++) {
+            int length = check(p1, list[i]);
+
+            if (length > 0) {
+                count++;
+                p1 += length - 1;
+                is_matched = 1;
+                break;
+            }
+        }
+
+        if (!is_matched) {
+            count++;
+        }
+    }
+
+    printf("%d", count);
+
+    return 0;
 }
