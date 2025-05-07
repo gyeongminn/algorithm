89b22dc30cacf0ed0fa2c6255da79b69c2a706db
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2025-05-08 01:00:40 +0900

    [Gold IV] Title: N-Queen, Time: 2288 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -4,23 +4,26 @@ using namespace std;
 int N, answer;
 
 // board 배열은 (1, 2) 좌표를 board[1]=2 와 같이 저장
-bool check(int row, int column, vector<int> &board) {
+bool check(int row, int column, vector<int>& board) {
     for (int r = 0; r < row; ++r) {
-        if (board[r] == column) return false;
+        if (board[r] == column)
+            return false;
         int row_offset = row - r, column_offset = column - board[r];
-        if (row_offset == abs(column_offset)) return false;
+        if (row_offset == abs(column_offset))
+            return false;
     }
     return true;
 }
 
-void solve(int row, vector<int> &board) {
+void solve(int row, vector<int>& board) {
     if (row == N) {
         answer++;
         return;
     }
 
     for (int column = 0; column < N; ++column) {
-        if (!check(row, column, board)) continue;
+        if (!check(row, column, board))
+            continue;
         board[row] = column;
         solve(row + 1, board);
     }
@@ -30,8 +33,22 @@ int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr);
 
     cin >> N;
-    vector<int> board(N + 1);
-    solve(0, board);
+    vector<int> board(N);
+
+    // 대칭인 부분 먼저 돌리고 *2
+    int mid = N / 2;
+    for (int c = 0; c < mid; c++) {
+        board[0] = c;
+        solve(1, board);
+    }
+    answer *= 2;
+
+    // N이 홀수라면 가운데 줄 처리 필요
+    if (N % 2 == 1) {
+        board[0] = mid;
+        solve(1, board);
+    }
+
     cout << answer;
 
     return 0;
