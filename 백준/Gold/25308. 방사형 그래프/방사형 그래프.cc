4eec90198b3cabba53b9b91b9c3a39e322cb7163
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2025-05-22 14:48:57 +0900

    [Gold IV] Title: 방사형 그래프, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/25308. 방사형 그래프/방사형 그래프.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> abilities(8);
+
+int solve(const vector<int> &indices) {
+    for (int i = 0; i < 8; ++i) {
+        int prev_idx = indices[(i + 7) % 8];
+        int prev = abilities[prev_idx];
+
+        int now_idx = indices[i];
+        int now = abilities[now_idx];
+
+        int next_idx = indices[(i + 1) % 8];
+        int next = abilities[next_idx];
+
+        long double lhs = (long double)now * (prev + next);
+        long double rhs = sqrtl(2.0L) * prev * next;
+        if (lhs <= rhs) return false;
+    }
+
+    return 1;
+}
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+    for (int &x : abilities) cin >> x;
+
+    vector<int> indices(8);
+    for (int i = 0; i < 8; ++i) indices[i] = i;
+
+    int answer = 0;
+    do {
+        answer += solve(indices);
+    } while (next_permutation(indices.begin(), indices.end()));
+    cout << answer;
+
+    return 0;
+}
