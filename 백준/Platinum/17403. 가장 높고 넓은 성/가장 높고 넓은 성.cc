7631812743bb362c4c9f5f16a239ed4d9cb21212
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2026-03-06 01:21:43 +0900

    [Platinum IV] Title: 가장 높고 넓은 성, Time: 12 ms, Memory: 2224 KB -BaekjoonHub

---
+++ 백준/Platinum/17403. 가장 높고 넓은 성/가장 높고 넓은 성.cc
@@ -0,0 +1,156 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+template<class T>
+struct Point {
+    T x, y;
+    int id = -1;
+
+    bool operator<(const Point &other) const {
+        if (x != other.x) return x < other.x;
+        return y < other.y;
+    }
+
+    bool operator==(const Point &other) const {
+        return x == other.x && y == other.y;
+    }
+};
+
+template<class T>
+__int128 polygon_area2(const vector<Point<T> > &poly) {
+    int n = static_cast<int>(poly.size());
+    if (n < 3) return 0;
+
+    __int128 sum = 0;
+    for (int i = 0; i < n; i++) {
+        int j = (i + 1) % n;
+        sum += static_cast<__int128>(poly[i].x) * poly[j].y - static_cast<__int128>(poly[i].y) * poly[j].x;
+    }
+    if (sum < 0) sum = -sum;
+    return sum;
+}
+
+template<class T>
+ll boundary_lattice_points(const vector<Point<T> > &hull) {
+    int n = static_cast<int>(hull.size());
+    if (n < 3) return 0;
+
+    ll B = 0;
+    for (int i = 0; i < n; i++) {
+        int j = (i + 1) % n;
+        ll dx = llabs(static_cast<ll>(hull[i].x) - static_cast<ll>(hull[j].x));
+        ll dy = llabs(static_cast<ll>(hull[i].y) - static_cast<ll>(hull[j].y));
+        B += std::gcd(dx, dy);
+    }
+    return B;
+}
+
+template<class T>
+static __int128 cross128(const Point<T> &a, const Point<T> &b, const Point<T> &c) {
+    return static_cast<__int128>(b.x - a.x) * (c.y - a.y) - static_cast<__int128>(b.y - a.y) * (c.x - a.x);
+}
+
+template<class T>
+bool inside_convex_ccw(const vector<Point<T> > &hull, const Point<T> &p) {
+    int n = static_cast<int>(hull.size());
+    if (n < 3) return false;
+    for (int i = 0; i < n; i++) {
+        int j = (i + 1) % n;
+        if (cross128(hull[i], hull[j], p) < 0) return false;
+    }
+    return true;
+}
+
+template<class T>
+vector<Point<T> > convex_hull(vector<Point<T> > pts, bool keep_collinear = false) {
+    sort(pts.begin(), pts.end());
+    pts.erase(unique(pts.begin(), pts.end()), pts.end());
+
+    int n = static_cast<int>(pts.size());
+    if (n <= 1) return pts;
+
+    auto should_pop = [&](const Point<T> &a, const Point<T> &b, const Point<T> &c) -> bool {
+        __int128 cr = cross128(a, b, c);
+        if (keep_collinear) return cr < 0;
+        return cr <= 0;
+    };
+
+    vector<Point<T> > lower, upper;
+    lower.reserve(n);
+    upper.reserve(n);
+
+    for (const auto &p: pts) {
+        while (static_cast<int>(lower.size()) >= 2 && should_pop(lower[static_cast<int>(lower.size()) - 2], lower.back(), p)) {
+            lower.pop_back();
+        }
+        lower.push_back(p);
+    }
+    for (int i = n - 1; i >= 0; --i) {
+        const auto &p = pts[i];
+        while (static_cast<int>(upper.size()) >= 2 && should_pop(upper[static_cast<int>(upper.size()) - 2], upper.back(), p)) {
+            upper.pop_back();
+        }
+        upper.push_back(p);
+    }
+
+    lower.pop_back();
+    upper.pop_back();
+    lower.insert(lower.end(), upper.begin(), upper.end());
+
+    if (keep_collinear) {
+        bool all_collinear = true;
+        for (int i = 2; i < static_cast<int>(pts.size()); i++) {
+            if (cross128(pts[0], pts[1], pts[i]) != 0) {
+                all_collinear = false;
+                break;
+            }
+        }
+        if (all_collinear) {
+            return pts;
+        }
+    }
+
+    return lower;
+}
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    vector<Point<ll> > alive;
+    for (int i = 0; i < n; i++) {
+        ll x, y;
+        cin >> x >> y;
+        alive.push_back(Point<ll>{x, y, i});
+    }
+
+    vector<int> answer(n, 0);
+    vector<int> mark(n, 0);
+    int stamp = 1;
+    int layer = 1;
+
+    while (static_cast<int>(alive.size()) >= 3) {
+        vector<Point<long long> > hull = convex_hull<ll>(alive, false);
+        if (static_cast<int>(hull.size()) < 3) break;
+
+        for (auto &p: hull) {
+            answer[p.id] = layer;
+            mark[p.id] = stamp;
+        }
+
+        vector<Point<ll> > next_alive;
+        next_alive.reserve(alive.size());
+        for (Point<ll> &p: alive) {
+            if (mark[p.id] != stamp) next_alive.push_back(p);
+        }
+        alive.swap(next_alive);
+
+        stamp++, layer++;
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << answer[i] << (i + 1 == n ? '\n' : ' ');
+    }
+}
